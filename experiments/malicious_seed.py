872110ab2b37d4782155b5c62473c8a775bc7201
Aditya Murthy <amurthy4@statestreet.com>
2018-09-21 17:39:58 -0400

    cleaned up malicious seed expt

--- experiments/malicious_seed.py
+++ experiments/malicious_seed.py
@@ -4,7 +4,7 @@ import numpy as np
 np.random.seed(1234)
 
 X_step = np.array([[x] for x in [-50.0, 25.0, -51.0, -52.0, -49.0,
-                                 24.0, 26.0, 41.0, 23.0,27.0, 22.0,
+                                 24.0, 26.0, 41.0, 23.0, 27.0, 22.0,
                                  20.0, 21.0, 28.0, 18.0, 17.0, 19.0,
                                  11.0, 10.0, 9.0, 5.0, 6.0, 4.0, 3.0,
                                  2.0, 1.0]])
@@ -16,19 +16,16 @@ Y_step = np.array([[y] for y in [36.0, 81.0, 2704.0, 2809.0, 2500.0,
                                  25.0, 9.0, 4.0, 1.0, 0.0]])
 
 domain = [{'name': 'var_1', 'type': 'discrete', 'domain': range(-100, 100)}]
-suggestions = None
+suggestions = np.array([[0]])
 
-for i in range(0,30):
-    if i == 28:
-        print('aha')
-    bo_step = GPyOpt.methods.BayesianOptimization(f=None,
-                                                  domain=domain,
-                                                  X=X_step,
-                                                  Y=Y_step,
-                                                  de_duplication=True)
+bo_step = GPyOpt.methods.BayesianOptimization(f=None,
+                                              domain=domain,
+                                              X=X_step,
+                                              Y=Y_step,
+                                              de_duplication=True)
+
+for i in range(0, 35):
     x_next = bo_step.suggest_next_locations(pending_X=suggestions)
-    if suggestions is None:
-        suggestions = x_next
-    else:
-        suggestions = np.vstack((suggestions, x_next))
+    suggestions = np.vstack((suggestions, x_next))
+
 print(suggestions.T)
